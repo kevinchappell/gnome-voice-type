d35cb0f1b567391d6be385d22c24248c3208d512
Kevin Chappell <kevin@chappell.dev>
2025-09-14 00:51:52 -0400

    add recording timeout

--- prefs.js
+++ prefs.js
@@ -59,6 +59,23 @@ export default class VoiceTypeInputPreferences extends ExtensionPreferences {
         });
         recordingGroup.add(qualityRow);
 
+        // Recording time limit setting
+        const timeLimitRow = new Adw.SpinRow({
+            title: _('Recording Time Limit'),
+            subtitle: _('Maximum recording duration in seconds'),
+            adjustment: new Gtk.Adjustment({
+                lower: 5,
+                upper: 300,
+                step_increment: 5,
+                page_increment: 10,
+                value: this.getSettings().get_int('recording-limit-seconds'),
+            }),
+        });
+        timeLimitRow.connect('notify::value', () => {
+            this.getSettings().set_int('recording-limit-seconds', timeLimitRow.get_value());
+        });
+        recordingGroup.add(timeLimitRow);
+
         // Create a preferences group for UI settings
         const uiGroup = new Adw.PreferencesGroup({
             title: _('Interface Settings'),
@@ -86,7 +103,7 @@ export default class VoiceTypeInputPreferences extends ExtensionPreferences {
 
         const usageRow = new Adw.ActionRow({
             title: _('Instructions'),
-            subtitle: _('Click the microphone icon in the top panel to start/stop recording. Transcribed text will be automatically typed at the cursor position.'),
+            subtitle: _('Click the microphone icon in the top panel to start/stop recording. Recording will automatically stop after the time limit. Transcribed text will be automatically typed at the cursor position.'),
         });
         infoGroup.add(usageRow);
 
@@ -95,5 +112,11 @@ export default class VoiceTypeInputPreferences extends ExtensionPreferences {
             subtitle: _('Enter the base URL (e.g., http://localhost:8675). The extension will automatically append "/transcribe" to this URL.'),
         });
         infoGroup.add(endpointInfoRow);
+
+        const timeLimitInfoRow = new Adw.ActionRow({
+            title: _('Recording Limit'),
+            subtitle: _('Set between 5-300 seconds. Longer recordings may use more memory and bandwidth.'),
+        });
+        infoGroup.add(timeLimitInfoRow);
     }
 }
